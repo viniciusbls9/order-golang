a233663f03a16274185f1f48b5f60b64f01cb098
Vinicius Benedito <beneditoloura@gmail.com>
2023-11-27 07:02:06 -0300

    test: ensure gets an error if tax is blank

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -15,3 +15,8 @@ func Test_If_It_Gets_An_Error_If_Price_Is_Blank(t *testing.T) {
 	order := Order{ID: "123"}
 	assert.Error(t, order.Validate(), "price must be greater than zero")
 }
+
+func Test_If_It_Gets_An_Error_If_Tax_Is_Blank(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0}
+	assert.Error(t, order.Validate(), "invalid tax")
+}
