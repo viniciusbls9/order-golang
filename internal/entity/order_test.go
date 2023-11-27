3d222b90a2d90a19f410014f024312434772c58d
Vinicius Benedito <beneditoloura@gmail.com>
2023-11-27 06:55:42 -0300

    test: ensure gets an error ir price is blank

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -10,3 +10,8 @@ func TestIfItGetsAnErrorIfIDIsBlank(t *testing.T) {
 	order := Order{}
 	assert.Error(t, order.Validate(), "ID is required")
 }
+
+func Test_If_It_Gets_An_Error_If_Price_Is_Blank(t *testing.T) {
+	order := Order{ID: "123"}
+	assert.Error(t, order.Validate(), "price must be greater than zero")
+}
