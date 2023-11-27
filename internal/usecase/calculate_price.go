90b665db192604cdca11ef07241922d557f4655f
Vinicius Benedito <beneditoloura@gmail.com>
2023-11-27 07:24:15 -0300

    feat: create calculate price usecase

---
+++ internal/usecase/calculate_price.go
@@ -0,0 +1,41 @@
+package usecase
+
+import "github.com/viniciusbls9/order-golang/internal/entity"
+
+type OrderInput struct {
+	ID    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutput struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type CalculateFinalPrice struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func (c *CalculateFinalPrice) Execute(input OrderInput) (*OrderOutput, error) {
+	order, err := entity.NewOrder(input.ID, input.Price, input.Tax)
+	if err != nil {
+		return nil, err
+	}
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		return nil, err
+	}
+	err = c.OrderRepository.Save(order)
+	if err != nil {
+		return nil, err
+	}
+	return &OrderOutput{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+}
