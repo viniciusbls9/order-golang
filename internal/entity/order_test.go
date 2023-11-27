d61737332d9f2a20a4f656eeda50fb490c7b5292
Vinicius Benedito <beneditoloura@gmail.com>
2023-11-27 07:02:21 -0300

    test: ensure final price is correct

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -20,3 +20,13 @@ func Test_If_It_Gets_An_Error_If_Tax_Is_Blank(t *testing.T) {
 	order := Order{ID: "123", Price: 10.0}
 	assert.Error(t, order.Validate(), "invalid tax")
 }
+
+func TestFinalPrice(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0, Tax: 1.0}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	order.CalculateFinalPrice()
+	assert.Equal(t, 11.0, order.FinalPrice)
+}
